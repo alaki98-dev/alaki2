eba085577eff77392c1b1bdc995e4fab722e97ae
alaki98-dev <159180584+alaki98-dev@users.noreply.github.com>
2024-03-17 08:34:15 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "MatFeaLib"
+copyright = "2024, MatFeaLib Developers"
+author = "MatFeaLib Developers"
 
 
 # -- General configuration ---------------------------------------------------
