14d48c0496708e0382a564f8b816c829c8a95383
alaki98-dev <159180584+alaki98-dev@users.noreply.github.com>
2024-04-06 14:33:05 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 project = 'limache'
 copyright = '2024, Developers'
-author = 'MatFeaLib Developers'
+author = ' Developers'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
